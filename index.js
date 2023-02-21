aad71fcfc3c774b569ac97ee36532f1ca2ddbd1e
GILSANGWOOK <125837947+GILSANGWOOK@users.noreply.github.com>
2023-02-21 10:35:32 +0900

    수정1

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-
-import Link from "next/link"
+import Link from 'next/link';
 export default function Home() {
   return (
     <>
-    <header>
-      <h1><Link href="/">WEB</Link></h1>
-    </header>
-    <ol>
-      <li><Link href="/read/1">html</Link></li>
-      <li><Link href="/read/2">css</Link></li>
-    </ol>
-
-    <article>
-      <h2>Welcome</h2>
-      Hello, WEB!!
-    </article>
-    <ul>
-      <li><Link href="/create">Create</Link></li>
-      <li><Link href="/update">Update</Link></li>
-      <li><Link href="/delete">Delete</Link></li>
-
-    </ul>
+      <h1>
+        <Link href="/">WEB</Link>
+      </h1>
+      <ol>
+        <li><Link href="/read/1">html</Link></li>
+        <li><Link href="/read/2">css</Link></li>
+      </ol>
+      <article>
+        <h2>Welcome</h2>
+        Hello, WEB2
+      </article>
+      <ul>
+        <li><Link href="/create">Create</Link></li>
+        <li><Link href="/update">Update</Link></li>
+        <li><Link href="/delete">Delete</Link></li>
+      </ul>
     </>
   )
 }
